306016add2b7b6fa7245ed5d034117ee699c1702
Satbir Singh <satbir126@gmail.com>
2019-09-21 10:24:51 -0400

    added the color scheme for nav and variable file.

--- elevate-app/src/app/base/base.module.ts
+++ elevate-app/src/app/base/base.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [],
-  exports: [],
-  declarations: [],
+  exports: [HeaderComponent],
+  declarations: [HeaderComponent],
   providers: [],
 })
 export class BaseModule { }
