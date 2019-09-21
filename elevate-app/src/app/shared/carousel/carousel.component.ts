6f34927bb15f9b7da77bfb380920cf715004bfbc
Ram <manikandan.it23@gmail.com>
2019-09-21 23:05:57 +0530

    Modal component added

--- elevate-app/src/app/shared/carousel/carousel.component.ts
+++ elevate-app/src/app/shared/carousel/carousel.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as M from 'materialize-css';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements OnInit, AfterViewInit {
+  ngAfterViewInit(): void {
+    var carousel = document.getElementById('carousel');
+    var instances = M.Carousel.init(carousel);
+  }
 
   constructor() { }
 
--- elevate-app/src/app/shared/share.module.ts
+++ elevate-app/src/app/shared/share.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { CarouselComponent } from './carousel/carousel.component';
-
+import {ModalComponent} from './modal/modal.component';
 
 @NgModule({
   imports: [],
-  exports: [SearchBoxComponent,CarouselComponent],
-  declarations: [SearchBoxComponent,CarouselComponent],
+  exports: [SearchBoxComponent,CarouselComponent,ModalComponent],
+  declarations: [SearchBoxComponent,CarouselComponent,ModalComponent],
   providers: [],
 })
 export class SharedModule { }
