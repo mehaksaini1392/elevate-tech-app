79b138ce7037543264ae7cc0ea580d8bf11ed985
Satbir Singh <satbir126@gmail.com>
2019-09-21 09:42:24 -0400

    added structure for the app

--- elevate-app/src/app/app.module.ts
+++ elevate-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseModule } from './base/base.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
