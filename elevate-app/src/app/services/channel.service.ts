f16694d9e793aa05cb655272a2a1cbe8309f0e6d
Mehak Saini <mehak@Mehaks-MacBook-Pro.local>
2019-09-21 17:12:07 -0400

    add/remove channel from cart obj

--- elevate-app/src/app/services/channel.service.ts
+++ elevate-app/src/app/services/channel.service.ts
@@ -9,12 +9,13 @@ import * as R from 'ramda';
 })
 export class ChannelService {
   private _allChannels: Channel[];
+  private shoppingCart = [];
   constructor(
     private http: HttpClient
 
   ) { }
 
-  getChannels(){
+  getChannels() {
     return this.http.get<Channel[]>('../../assets/data/channels.json').pipe(
       tap((resp) => {
         this._allChannels = resp;
@@ -23,13 +24,24 @@ export class ChannelService {
     );
   }
 
-  getAllChannels(){
+  getAllChannels() {
     return this._allChannels;
   }
 
-  getChannelsFilteredByName(searchText){
-    return R.filter((channel: Channel)=>{
+  getChannelsFilteredByName(searchText) {
+    return R.filter((channel: Channel) => {
       return channel.name.toLowerCase().indexOf(searchText.toLowerCase()) > -1;
     }, this._allChannels);
   }
+
+  addChannelToCart(channelId) {
+    const channelObj = R.find(R.propEq('ttCode', channelId))(this._allChannels);
+    this.shoppingCart.push(channelObj);
+  }
+
+  removeChannelFromCart(channelId) {
+    const indexToRemove = R.findIndex(R.propEq('ttCode', channelId))(this.shoppingCart);
+    R.remove(indexToRemove, this.shoppingCart);
+  }
+
 }
